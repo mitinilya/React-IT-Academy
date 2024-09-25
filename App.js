93e67220e9c92141a873f1f0ec632dcfcf6136d5
Ilya Mitin <ilyamitin24060331@gmail.com>
2024-09-25 17:37:12 +0300

    ishop0

---
+++ App.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Warning from './components/Shop';
+
+ReactDOM.render(
+  <Warning name="Магазин Автотоваров" address = "г. Минск, ул. Лермонтова д.35" />,
+  document.getElementById('container')
+);
---
+++ components/Shop.js
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+
+import './Shop.css';
+
+class Warning extends React.Component {
+
+  render() {
+    return (
+      <div className='Warning'>
+        <span className='Warning_Text'>
+          {this.props.name},
+          {this.props.address}
+        </span>
+      </div>
+    );
+  }
+
+}
+
+export default Warning;
