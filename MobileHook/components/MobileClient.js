3510a3cacc575b57609c35993de34940fae317f5
Ilya Mitin <ilyamitin24060331@gmail.com>
2024-11-08 15:36:08 +0300

    MobileHook

---
+++ MobileHook/components/MobileClient.js
@@ -0,0 +1,74 @@
+﻿import React, {useRef, useState, useMemo, useEffect} from 'react';
+import PropTypes from 'prop-types';
+import myEvent from './events';
+
+import './MobileClient.css';
+
+export const MobileClient = React.memo(({infoClient}) => {
+
+  // state = {
+  //   isEditing : false,
+  // }
+
+  const [isEditing, setIsEditing] = useState (false);
+
+  const famRef = useRef (null);
+  const imRef = useRef (null);
+  const otchRef = useRef (null);
+  const balanceRef = useRef (null);
+
+
+
+  const onEdit = () => {
+    setIsEditing(!isEditing);
+  }
+
+  const save = () => {
+    myEvent.emit('updateClient', {
+      id: infoClient.id,
+      fam : famRef.current.value,
+      im: imRef.current.value,
+      otch: otchRef.current.value,
+      balance : parseFloat(balanceRef.current.value)
+  })
+  setIsEditing (false);
+  }
+
+  const deleteClient = () => {
+    myEvent.emit('deleteClient', infoClient.id);
+  };
+  
+
+
+    console.log("MobileClient id="+ infoClient.id +" render");
+    const status = infoClient.balance >= 0 ? 'active' : "blocked";
+    return (
+      <tr>
+      {isEditing ? (
+        <React.Fragment>
+          <td><input ref = {famRef} defaultValue = {infoClient.fam}/></td>
+          <td><input ref = {imRef} defaultValue={infoClient.im}/></td>
+          <td><input ref = {otchRef} defaultValue = {infoClient.otch}/></td>
+          <td><input ref = {balanceRef} defaultValue = {infoClient.balance}/></td>
+          <td>{status}</td>
+          <td><input type = "button" value = "Сохранить" onClick = {save}/></td>
+          <td><input type = "button" value = "Удалить" onClick = {deleteClient}/></td>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <td>{infoClient.fam}</td>
+          <td>{infoClient.im}</td>
+          <td>{infoClient.otch}</td>
+          <td>{infoClient.balance}</td>
+          <td style={{ backgroundColor: status === 'active' ? 'green' : 'red', color: 'white' }}>{status}</td>
+          <td><input type = "button" value = "Редактировать" onClick = {onEdit}/></td>
+          <td><input type = "button" value = "Удалить" onClick = {deleteClient}/></td>
+        </React.Fragment>    
+      )
+    }
+    </tr>
+    )
+
+  }
+)
+
---
+++ MobileHook/components/MobileCompany.js
@@ -0,0 +1,114 @@
+﻿import React, {useState, useMemo, useEffect, useCallback} from 'react';
+
+
+import {MobileClient} from './MobileClient';
+import myEvent from './events';
+import './MobileCompany.css';
+
+export const MobileCompany = ({clients: initClients}) =>
+{
+  // static propTypes = {
+   
+  //   clients:PropTypes.arrayOf(
+  //     PropTypes.shape({
+  //       id: PropTypes.number.isRequired,
+  //       fam: PropTypes.string.isRequired,
+  //       im: PropTypes.string.isRequired,
+  //       otch: PropTypes.string.isRequired,
+  //       balance: PropTypes.number.isRequired,
+  //     })
+      
+  //   ),
+  // };
+
+  const [clients, setClients] = useState (initClients);
+  const [filter, setFilter] = useState ('all');
+  
+
+  // componentDidMount() {
+  //   myEvent.addListener('updateClient', this.updateClient);
+  //   myEvent.addListener('deleteClient', this.deleteClient);
+  //   myEvent.addListener('addClient', this.addClient);
+  // }
+
+  // componentWillUnmount() {
+  //   myEvent.removeListener('updateClient', this.updateClient);
+  //   myEvent.removeListener('deleteClient', this.deleteClient);
+  //   myEvent.removeListener('addClient', this.addClient);
+  // }
+
+     const addClient = useCallback((newClient) => {
+        let newClients = clients.slice();
+        newClients.push(newClient);
+        setClients (newClients);
+     }, [clients]);
+      const deleteClient = useCallback((clientId) => {
+        let newClients = clients.slice().filter (c=> (c.id !== clientId));
+        setClients (newClients);
+      }, [clients]);
+
+      const updateClient = useCallback((updatedClient) => {
+        let newClients = clients.slice();
+
+            newClients.forEach((c, i) => {
+                if (c.id === updatedClient.id) {
+                  newClients[i] = {...updatedClient};
+      }})
+      setClients(newClients);
+    }, [clients]);
+   
+    useEffect(
+      () => {
+        myEvent.addListener('updateClient', updateClient);
+        myEvent.addListener('deleteClient', deleteClient);
+        myEvent.addListener('addClient', addClient);
+        return () => {
+          myEvent.removeListener('updateClient', updateClient);
+          myEvent.removeListener('deleteClient', deleteClient);
+          myEvent.removeListener('addClient', addClient);
+        }
+      }, [updateClient,addClient,deleteClient]);
+      
+
+  const getFilteredClients = useMemo (
+    () => {
+      if (filter === 'all') return clients;
+      if (filter === 'active') return clients.filter (c=>(c.balance>=0));
+      if (filter === "blocked") return clients.filter (c=>(c.balance < 0));
+    }, [filter,clients]);
+
+    console.log("MobileCompany render");
+
+    return (
+      <div>
+        <div>
+          <input type = "button" value = "Все" onClick = {()=>setFilter('all')}/>
+          <input type = "button" value = "Актівные" onClick = {()=>setFilter('active')}/>
+          <input type = "button" value = "Заблокированные" onClick = {()=>setFilter('blocked')}/>
+        </div>
+        <table>
+          <thead>
+            <tr>
+            <th>Фамилия</th>
+            <th>Имя</th>
+            <th>Отчество</th>
+            <th>Баланс</th>
+            <th>Статус</th>
+            <th>Редактировать</th>
+            <th>Удалить</th>
+            </tr>
+          </thead>
+          <tbody>
+          
+            {getFilteredClients.map(c => (
+              <MobileClient key = {c.id} infoClient = {c}/>
+            ))}
+           
+          </tbody>
+        </table>
+        <input type = "button" value = "Добавить клиента" onClick = {()=> myEvent.emit('addClient', {id: Date.now(0), fam:"Владиславский", im: "Владислав", otch:"Владиславович", balance : 100})}/>
+      </div>
+      
+    )
+
+  };
